79bc6cdeb8d485a10810be46371dac0ab6443e2e
Azamat <azamat201760@yandex.ru>
2025-11-28 13:21:51 +0300

    [add] load base promt in settings

--- adapter/rest/di.py
+++ adapter/rest/di.py
@@ -7,20 +7,14 @@ from usecase.answer import AnswerUsecase
 from usecase.interface import LLMProviderRepository
 
 
-def get_system_promt() -> str:
-    with open('promt/system.txt') as f:
-        return f.read()
-
-
 def get_llm_config(
-    promt: str = Depends(get_system_promt),
     settings: Settings = Depends(get_settings),
 ) -> LLMConfig:
     return LLMConfig(
         base_url='https://rest-assistant.api.cloud.yandex.net/v1',
         api_key=settings.api_key,
         model=f'gpt://{settings.folder_id}/qwen3-235b-a22b-fp8/latest',
-        system_prompt=promt,
+        system_prompt=settings.base_prompt,
         folder_id=settings.folder_id,
     )
 
--- adapter/rest/settings.py
+++ adapter/rest/settings.py
@@ -1,6 +1,14 @@
 from functools import lru_cache
 
+from fastapi import Path
+from pydantic import Field
 from pydantic_settings import BaseSettings
+
+BASE_PROMPT_PATH = Path('promt/system.txt')
+
+
+def load_base_prompt() -> str:
+    return BASE_PROMPT_PATH.read_text(encoding='utf-8')
 
 
 class Settings(BaseSettings):
@@ -9,6 +17,8 @@ class Settings(BaseSettings):
     dev: bool
     api_key: str
     folder_id: str
+
+    base_prompt: str = Field(default_factory=load_base_prompt)
 
     class Config:
         env_file = '.env'
