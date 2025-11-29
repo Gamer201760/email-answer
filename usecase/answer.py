a545afa0ea2a6e4e1a06f38d3aae5b0716811f4d
Azamat <azamat201760@yandex.ru>
2025-11-29 11:45:41 +0300

    [add] email policy

--- usecase/answer.py
+++ usecase/answer.py
@@ -1,8 +1,14 @@
-from openai import OpenAI
+from openai import BaseModel, OpenAI
 from requests import Session
 
 from domain.llmconfig import LLMConfig
 from domain.models import AnswerResponse
+
+
+class LAnswer(BaseModel):
+    result: str
+    depertament: str
+    importance: int
 
 
 class AnswerUsecase:
@@ -11,22 +17,30 @@ class AnswerUsecase:
         self._config = config
         self._session = Session()
 
-    def execute(self, text: str) -> AnswerResponse:
+    def execute(self, text: str) -> AnswerResponse | None:
         response = self._session.post(
-            "localhost:8123/query", data={"query": text, "top_k": 3}
+            "http://localhost:8123/query", json={"query": text, "top_k": 3}
         )
+        if response.status_code != 200:
+            context = ""
+        else:
+            context = response.json()["context"]
 
-        res = self._client.responses.create(
+        dobavka = '\nЕще определи что это за департамент: Finance HR Marketing Sales Product Engineering и важность этого письма от 0 до 10, формат ответа {"result": текст ответа, "depertament": департамент, "importance": важность [0, 10]}'
+
+        res = self._client.responses.parse(
+            text_format=LAnswer,
             model=self._config.model,
             instructions=self._config.system_prompt,
-            input=response.json()["context"] + text,
+            input=context + text + dobavka,
             temperature=self._config.temperature,
             top_p=self._config.top_p,
             max_output_tokens=self._config.max_tokens,
         )
-        return AnswerResponse(
-            result=res.output_text,
-            tokens=res.usage.total_tokens if res.usage else 0,
-            reply_to=["azamat201760@ya.ru"],
-        )
-
+        if res.output_parsed:
+            print(res.output_parsed.depertament, res.output_parsed.importance)
+            return AnswerResponse(
+                result=res.output_parsed.result,
+                tokens=res.usage.total_tokens if res.usage else 0,
+                reply_to=["azamat201760@ya.ru"],
+            )
